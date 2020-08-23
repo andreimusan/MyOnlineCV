ec8a52de488fb21fb9bb532fdfd88b82ef7652a6
andrei <andreimusan_13@ymail.com>
2020-08-23 16:16:50 +0300

    send email works

--- js/app.js
+++ js/app.js
@@ -270,147 +270,16 @@ function carousel1(){
 carousel1();
 
 // send form to email
-// const sendBtn = document.getElementById('contact-submit');
-// sendBtn.addEventListener('click', function(event) {
-//     event.preventDefault();
-    
-//     emailjs.sendForm('gmail', 'template_Andrei', '#contact-form', 'user_H7c01DIrLpTuRGbCwvkrq')
-//         .then(function(response) {
-//            console.log('SUCCESS!', response.status, response.text);
-//         }, function(error) {
-//            console.log('FAILED...', error);
-//         });
-        
-// });
-
 function submitForm() {
     event.preventDefault();
     
-    emailjs.sendForm('gmail', 'template_Andrei', '#contact-form', 'user_H7c01DIrLpTuRGbCwvkrq')
+    emailjs.sendForm('gmail', 'andrei_template', '#contact-form', 'user_H7c01DIrLpTuRGbCwvkrq')
         .then(function(response) {
            console.log('SUCCESS!', response.status, response.text);
+           alert('Your message was successfully sent! I will get back to you as soon as possible.')
+           document.getElementById("contact-form").reset();
         }, function(error) {
            console.log('FAILED...', error);
+           alert('Your message was not sent! Please try again.')
         });
 }
-
-// // carousel 2
-// function carousel2(){
-//     let itemClassName = "carousel-photo2",
-//         items = document.getElementsByClassName(itemClassName),
-//         totalItems = items.length,
-//         slide = 0,
-//         moving = true;
-
-//     // Set classes
-//     function setInitialClasses() {
-//         // Targets the previous, current, and next items. This assumes there are at least three items.
-//         items[totalItems - 1].classList.add("prev");
-//         items[0].classList.add("active");
-//         items[1].classList.add("next");
-//     }
-
-//     // Set event listeners
-//     function setEventListeners() {
-//         let next = document.getElementsByClassName('carousel-button-next2')[0],
-//             prev = document.getElementsByClassName('carousel-button-prev2')[0];
-
-//         next.addEventListener('click', moveNext);
-//         prev.addEventListener('click', movePrev);
-//     }
-
-//     // Next navigation handler
-//     function moveNext() {
-//         // Check if moving
-//         if (!moving) {
-//             // If it's the last slide, reset to 0, else +1
-//             if (slide === (totalItems - 1)) {
-//                 slide = 0;
-//             } else {
-//                 slide++;
-//             }
-//             // Move carousel to updated slide
-//             moveCarouselTo(slide);
-//         }
-//     }
-
-//     // Previous navigation handler
-//     function movePrev() {
-//         // Check if moving
-//         if (!moving) {
-//         // If it's the first slide, set as the last slide, else -1
-//             if (slide === 0) {
-//                 slide = (totalItems - 1);
-//             } else {
-//                 slide--;
-//             }
-                    
-//             // Move carousel to updated slide
-//             moveCarouselTo(slide);
-//         }
-//     }
-
-//     function disableInteraction() {
-//         // Set 'moving' to true for the same duration as our transition.
-//         // (0.5s = 500ms)
-//         moving = true;
-
-//         // setTimeout runs its function once after the given time
-//         setTimeout(function(){
-//             moving = false
-//         }, 500);
-//     }
-
-//     function moveCarouselTo(slide) {
-//         // Check if carousel is moving, if not, allow interaction
-//         if(!moving) {
-//             // temporarily disable interactivity
-//             disableInteraction();
-//             // Update the "old" adjacent slides with "new" ones
-//             let newPrevious = slide - 1,
-//                 newNext = slide + 1,
-//                 oldPrevious = slide - 2,
-//                 oldNext = slide + 2;
-
-//             // Test if carousel has more than three items
-//             if ((totalItems - 1) > 3) {
-//                 // Checks and updates if the new slides are out of bounds
-//                 if (newPrevious <= 0) {
-//                     oldPrevious = (totalItems - 1);
-//                 } else if (newNext >= (totalItems - 1)){
-//                     oldNext = 0;
-//                 }
-
-//                 // Checks and updates if slide is at the beginning/end
-//                 if (slide === 0) {
-//                     newPrevious = (totalItems - 1);
-//                     oldPrevious = (totalItems - 2);
-//                     oldNext = (slide + 1);
-//                 } else if (slide === (totalItems -1)) {
-//                     newPrevious = (slide - 1);
-//                     newNext = 0;
-//                     oldNext = 1;
-//                 }
-
-//                 // Now we've worked out where we are and where we're going, by adding/removing classes we'll     trigger the transitions. Reset old next/prev elements to default classes
-//                 items[oldPrevious].className = itemClassName;
-//                 items[oldNext].className = itemClassName;
-
-//                 // Add new classes
-//                 items[newPrevious].className = itemClassName + " prev";
-//                 items[slide].className = itemClassName + " active";
-//                 items[newNext].className = itemClassName + " next";
-//             }
-//         }
-//     }
-
-//     function initCarousel() {
-//         setInitialClasses();
-//         setEventListeners();
-//         // Set moving to false so that the carousel becomes interactive
-//         moving = false;
-//     }
-
-//     initCarousel();
-// };
-// carousel2();
